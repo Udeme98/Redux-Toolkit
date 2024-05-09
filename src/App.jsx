9e9a5b89e25921f5249c4bf5fc01abd0282ec9af
Udeme98 <Darealudeme1@gmail.com>
2024-05-09 13:03:29 +0100

    added amount and total functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+calculateTotals;
 
 const App = () => {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
       <Navbar />
